5bff5958efde4a829a86831ce6087db7c8dee6ea
nidalasx <saladin.nidalasx@gmail.com>
2022-07-31 20:50:37 +0200

    revised all of the code

--- rotation_cypher.py
+++ rotation_cypher.py
@@ -1,44 +1,33 @@
 import string
 
-from rx import return_value
-from values import *
-from functions import *
+user_inputstring = input("What do you want to encrypt?:")
+user_inputkey = input("What is your key?:")
+user_inputkey = int(user_inputkey)
+
+ALPHABET = string.ascii_lowercase
+alphabet_list = []
+numbers_list = []
+
+def indvchar_list(x, y):
+    for line in x:
+        for c in line:
+            y.append(c)
+
+indvchar_list(ALPHABET, alphabet_list)
 
 
-user_message = input("What do you want to encrypt:")
+for x in user_inputstring:
+   numbers_list.append(ord(x) - 96)
 
 
-input_key = input("What is your key:")
-KEY = int(input_key)
+for idx, el in enumerate(numbers_list):
+    if el is not None:
+        numbers_list[idx] += user_inputkey
 
 
+user_inputstring = [alphabet_list[i] for i in numbers_list]
 
-indvchar_list(ALPHABET, alphabet_characters)
-indvchar_list(user_message, user_list)
+encrypted_string = "".join(user_inputstring)
 
-user_length = len(user_list)
-
-deleted_chars = alphabet_characters[0:KEY]
-
-del alphabet_characters[0:KEY]
-
-alphabet_characters.extend(deleted_chars)
-
-encrypted_message = alphabet_characters[0:user_length]
-
-
-print("This is your encrypted message")
-print(encrypted_message)
-
-
-
-
-
-
-
-
-
-
-
-
+print(encrypted_string)
 
